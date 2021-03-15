acd50f399cdea1c7819855ab538d867c78a8c1a8
Hezron-Nelapati <nelapatihezron83@gmail.com>
2021-03-16 01:16:31 +0530

    Blog initial

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+    return (
+        <View>
+            <Text>Welcome To Index Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
